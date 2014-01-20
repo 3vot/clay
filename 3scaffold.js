2109214958dc8ea396c04dcd4e4666094bc785f3
Roberto Rodriguez <roberto@rodriguezartavia.com>
2014-01-20 10:22:19 -0600

    stopped using path with scafffold

--- 3scaffold.js
+++ 3scaffold.js
@@ -11,22 +11,20 @@ _3scaffold = (function() {
 
   _3scaffold.setup= function (options){
 
-    fs.mkdir( Path.join(process.cwd(), options.folder ), function(){});
-    fs.mkdir( Path.join(process.cwd(), options.folder , "apps" ), function(){});
-    fs.mkdir( Path.join(process.cwd(), options.folder , "apps", "dependencies" ), function(){});
-    fs.mkdir( Path.join(process.cwd(), options.folder , "tmp" ), function(){});
-    
-    var _3votJSON = require( Path.join( __dirname, "templates", "3vot.json"  ));
-    var gitIgnore = fs.readFileSync(  Path.join( __dirname, "templates", ".gitignore"  ));
-    var pckJSON = require( Path.join(__dirname, "templates", "package.json"  ));
-    
+    fs.mkdir( Path.join( process.cwd(), options.folder ), function(){} );
+    fs.mkdir( Path.join( process.cwd(), options.folder , "apps" ), function(){} );
+    fs.mkdir( Path.join( process.cwd(), options.folder , "apps", "dependencies" ), function(){} );
+    fs.mkdir( Path.join( process.cwd(), options.folder , "tmp" ), function(){}) ;
+
+    var _3votJSON = require( "./templates/3vot.json"  );
+    var gitIgnore = fs.readFileSync(  "./templates/.gitignore"  );
+    var pckJSON = require( "./templates/package.json"  );
+
     _3votJSON.key = options.key;
     _3votJSON.profile = options.profile;
-    
+
     fs.writeFileSync( Path.join(process.cwd(), options.folder, "3vot.json"), JSON.stringify(_3votJSON) );
-
     fs.writeFileSync( Path.join(process.cwd(), options.folder, "package.json"), JSON.stringify(pckJSON) );
-
     fs.writeFileSync( Path.join(process.cwd(), options.folder, ".gitignore"), gitIgnore );
 
   }
