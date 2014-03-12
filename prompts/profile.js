3bd429edc01462e70ab676c2717b2bd8c04e80fc
Roberto Rodriguez <roberto@rodriguezartavia.com>
2014-03-12 12:35:36 -0600

    promp profile reading 3vot.json

--- prompts/profile.js
+++ prompts/profile.js
@@ -26,8 +26,7 @@ function create(){
     { name: 'user_name', description: 'username: ( The username, that appears in the url )' }, 
     { name: 'email', description: 'email: ( Your Email, required in order to administer your profile )' }],
     function (err, result) {
-      LoadPackage(result)
-      .then(Create)
+      Create()
       .then( function(profile){ console.log("Profile Created Succesfully".green); console.log( ( "Save your developer key: " + profile.security.public_dev_key ).bold) } )
       .fail( function(err){ console.log("Error creating Profile".red.bold); console.error(err.red); } )
   });
